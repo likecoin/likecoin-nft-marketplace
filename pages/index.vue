88621f7adf9fe8a274280fe4fed49379808cc0ec
William Chong <me@williamchong.cloud>
2022-12-15 00:37:39 +0800

    💄 Add basic pages

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <form @submit.prevent="onSearchClassId">
+      <input v-model="classIdText" placeholder="Search NFT Class ID">
+      <input type="submit" value="Search" />
+    </form>
+  </div>
+</template>
+
+<script setup lang="ts">
+const router = useRouter()
+const classIdText = ref('');
+
+function onSearchClassId() {
+  router.push({ path: `/nft/${classIdText.value}` });
+}
+
+</script>
