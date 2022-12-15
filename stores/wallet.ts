d9ba5f4d244c91e8bcde91695736734b9c0dbd55
William Chong <me@williamchong.cloud>
2022-12-16 01:08:44 +0800

    ✨ Add connect wallet header and store

---
+++ stores/wallet.ts
@@ -0,0 +1,37 @@
+import { defineStore } from "pinia";
+import {
+  Keplr,
+  Window as KeplrWindow,
+  KeplrSignOptions,
+  AccountData,
+  OfflineAminoSigner,
+  OfflineDirectSigner,
+} from "@keplr-wallet/types";
+import network from "@/constant/network";
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
+  interface Window extends KeplrWindow {}
+}
+
+export const useWalletStore = defineStore("counter", () => {
+  const accounts = ref([] as readonly AccountData[]);
+  const signer = ref(null as (OfflineAminoSigner & OfflineDirectSigner | null));
+
+  const wallet = computed(() => accounts.value[0]?.address);
+  const isConnected = computed(() => !!wallet);
+
+  async function connect() {
+    if (!window.keplr) return;
+    const { keplr } = window;
+    await keplr.experimentalSuggestChain(network);
+    await keplr.enable(network.chainId);
+    if (!window.getOfflineSigner) throw new Error('CANNOT_FIND_OFFLINE_SIGNER');
+    const offlineSigner = window.getOfflineSigner(network.chainId);
+    signer.value = offlineSigner;
+    const acc = await offlineSigner.getAccounts();
+    accounts.value = acc;
+  }
+
+  return { accounts, signer, wallet, isConnected, connect };
+});
