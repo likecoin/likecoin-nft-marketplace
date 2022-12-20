8a0c066bfc06d35011436e812a9b1eb3c3d3b42e
William Chong <me@williamchong.cloud>
2022-12-20 21:01:54 +0800

    💄 Add nav header

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,19 @@
 <template>
   <div>
-    <WalletHeader />
+    <header>
+      <div style="display: flex">
+        <h1>LikeCoin NFT Marketplace</h1>
+        <div style="flex: 1"></div>
+        <WalletHeader />
+      </div>
+      <NavHeader />
+    </header>
     <slot />
   </div>
 </template>
+<style scoped>
+
+h1 {
+    display: inline-block;
+}
+</style>
