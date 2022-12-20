b49cef96ecc04cd1faa1dc9e5388e17af93b2f04
William Chong <me@williamchong.cloud>
2022-12-20 18:14:42 +0800

    ✨ Add recent event in index

--- constant/index.ts
+++ constant/index.ts
@@ -2,4 +2,5 @@ export const IS_TESTNET = false;
 
 export const CHAIN_ID = IS_TESTNET ? 'likecoin-public-testnet-5' : 'likecoin-chain-2'
 export const RPC_URL = IS_TESTNET ? 'https://node.testnet.like.co/rpc/' : 'https://mainnet-node.like.co/rpc/';
+export const LCD_URL = IS_TESTNET ? 'https://node.testnet.like.co/' : 'https://mainnet-node.like.co/';
 export const LIKER_LAND_HOST = `${IS_TESTNET ? 'rinkeby.' : ''}liker.land`;
--- pages/index.vue
+++ pages/index.vue
@@ -11,12 +11,41 @@
       <h2>View and Sell NFT</h2>
       <button @click="onViewOwned">View my NFTs</button>
     </section>
+    <section v-if="listingEvents.length">
+      <h2>Recent NFT Listings</h2>
+      <ul>
+        <li v-for="i in listingEvents">
+          <NuxtLink :to="`/listings/${i.class_id}`">
+            {{ i.class_id }} | {{ i.nft_id }} | {{ new BigNumber(i.price).shiftedBy(-9).toFixed() }}LIKE
+          </NuxtLink>
+        </li>
+      </ul>
+    </section>
+    <section v-if="buyNFTEvents.length">
+      <h2>Recent NFT Purchases</h2>
+      <li v-for="i in buyNFTEvents">
+        <NftLink :class-id="i.class_id" /> |
+        <NftLink :class-id="i.class_id" :nft-id="i.nft_id" /> | {{ new BigNumber(i.price).shiftedBy(-9).toFixed() }}LIKE
+      </li>
+    </section>
   </div>
 </template>
 
 <script setup lang="ts">
+import BigNumber from 'bignumber.js';
+
 const router = useRouter();
 const classIdText = ref('');
+
+const listingEvents = ref([] as any[]);
+const buyNFTEvents = ref([] as any[]);
+
+onMounted(async () => {
+  [listingEvents.value, buyNFTEvents.value] = await Promise.all([
+    getRecentListingEvents(),
+    getRecentBuyNFTEvents(),
+  ]);
+})
 
 function onSearchClassId() {
   router.push({ path: `/listings/${classIdText.value}` });
