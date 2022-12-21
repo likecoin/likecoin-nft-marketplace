b89977ef0c2ad572b7d0c6cce1fdc22152b9c123
William Chong <me@williamchong.cloud>
2022-12-21 15:15:17 +0800

    🔍️ Add title and og data

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: [
-    "@pinia/nuxt",
-  ],
+  app: {
+    head: {
+      title: "LikeCoin NFT Marketplace",
+      meta: [
+        { property: "og:title", content: "LikeCoin NFT Marketplace" },
+        {
+          name: "description",
+          content:
+            "Marketplace for all NFT on LikeCoin chain using x/likenft chain module.",
+        },
+        {
+          property: "og:description",
+          content:
+            "Marketplace for all NFT on LikeCoin chain using x/likenft chain module.",
+        },
+        {
+          property: "og:image",
+          content: "https://liker.land/images/og/writing-nft.jpg",
+        },
+        { name: "theme-color", content: "#28646e" },
+      ],
+    },
+  },
+  modules: ["@pinia/nuxt"],
 });
