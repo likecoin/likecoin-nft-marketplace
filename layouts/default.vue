ab804a62825ea28109c309bb672131c8a8a7860d
William Chong <me@williamchong.cloud>
2023-01-13 15:21:22 +0800

    🔍️ Add footer link

--- layouts/default.vue
+++ layouts/default.vue
@@ -9,6 +9,12 @@
       <NavHeader />
     </header>
     <slot />
+    <footer>
+      <a href="https://liker.land">Writing NFT</a>
+      <a href="https://likecoin.github.io/likecoin-nft-dashboard/">NFT Dashboard</a>
+      <a href="https://github.com/likecoin/likecoin-nft-marketplace">Github</a>
+      <a href="https://like.co">About LikeCoin</a>
+    </footer>
   </div>
 </template>
 <style>
@@ -22,4 +28,13 @@ th {
   border-collapse: collapse;
   border: 1px solid;
 }
+
+footer {
+  margin: 5px;
+}
+
+footer a {
+  margin-left: 10px;
+}
+
 </style>
